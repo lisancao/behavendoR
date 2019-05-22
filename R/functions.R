206817febc3055928d90ac4d8b419e865b6ac485
Lisa N. Cao <20377512+lisancao@users.noreply.github.com>
2019-05-21 22:08:00 -0700

    added initial files
    only installs so far

---
+++ R/functions.R
@@ -0,0 +1,18 @@
+install.packages('tidyverse')
+
+install.packages('qualtRics')
+install.packages('psych')
+
+install.packages('gendercodeR')
+
+
+##Hormone Levels 
+
+##Voice analysis protocols 
+
+##Face Ratings 
+
+##Survey Data 
+
+##Models 
+
