fb165b350d29afd224d19ecc51bdd9187ca94a4f
Lisa N. Cao <20377512+lisancao@users.noreply.github.com>
2019-05-24 16:17:19 -0700

    added dependencies

---
+++ R/main_functions.R
@@ -0,0 +1,51 @@
+install.packages('devtools')
+
+install.packages('foreign')
+
+install.packages('tidyverse')
+
+install.packages('qualtRics')
+install.packages('psych')
+install.packages('lme4')
+install.packages('nlme')
+
+install.packages('lubridate')
+install.packages('stringr')
+install.packages('forcats')
+
+
+install.packages('gendercodeR')
+
+devtools::install_github("brentthorne/posterdown")
+
+tinytex::install_tinytex()
+
+install.packages('gganimate')
+install.packages('RColorBrewer')
+
+
+#load libraries
+library('tidyverse')
+library('foreign')
+library('qualtRics')
+library('psych')
+library('lme4')
+library('nlme')
+library('gendercodeR')
+library('lubridate')
+library('stringr')
+library('forcats')
+library('posterdown')
+library('gganimate')
+library('RColorBrewer')
+
+##Hormone Levels
+
+##Voice analysis protocols
+
+##Face Ratings
+
+##Survey Data
+
+##Models
+
